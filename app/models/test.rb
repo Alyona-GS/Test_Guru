70849ba805f7a300a0b7ffdce4ac9362126e4d70
Alyona-GS <asgor1994@gmail.com>
2022-02-18 20:40:05 +0400

    feat: add tests.all_from(category)

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.all_from (category)
+    category_id = Category.find_by(title: category).id
+    self.where(category_id: category_id).pluck(:title).sort.reverse
+  rescue StandardError => e
+    puts "Category hasn't been found"
+  end
 end
