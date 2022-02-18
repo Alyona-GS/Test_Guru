100e7d0b93181260ef89e8621264d265d7e0f81c
Alyona-GS <asgor1994@gmail.com>
2022-02-18 23:09:21 +0400

    feat: add user.all_tests(level)

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,7 @@
 class Test < ApplicationRecord
+
+  has_and_belongs_to_many :users
+
   def self.all_from (category)
     category_id = Category.find_by(title: category).id
     self.where(category_id: category_id).pluck(:title).sort.reverse
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-User.create(name: "Asgor", email: "my_email@mail.com")
+User.create([
+  { name: "Asgor", email: "my_email@mail.com" },
+  { name: "Asmos", email: "my_email@mail.ru" } ])
 
 Category.create([
   { title: 'Ruby' },
@@ -19,16 +21,20 @@ Question.create([
   { body: 'What is the main loop used for dictionaries?',  test_id: 4 },
   { body: 'Do procs have a similar behaviour as lambdas?', test_id: 5 } ])
 
-Answer. create([
+Answer.create([
   {body: "Yes",       correct: true, question_id: 1 },
   {body: "Yes",       correct: true, question_id: 2 },
   {body: "Bold font", correct: true, question_id: 3 },
   {body: "For...in",  correct: true, question_id: 4 },
   {body: "Yes, but lambda is more strict wuth arguments", correct: true, question_id: 5 },
-  
+
   {body: "No",      question_id: 1 },
   {body: "No",      question_id: 2 },
   {body: "Cursive", question_id: 3 },
   {body: "While",   question_id: 4 },
   {body: "Procs is an alias of lambda", question_id: 5 },
 ])
+
+User.first.tests << Test.find(1)
+User.first.tests << Test.find(3)
+User.last.tests << Test.find(2)
