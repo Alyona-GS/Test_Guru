fa55bb4b37820fbdab4ee16b7afbe9b7efdea64b
Alyona-GS <asgor1994@gmail.com>
2022-02-20 17:16:30 +0400

    feat: add foreign keys to tests_users

--- db/schema.rb
+++ db/schema.rb
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(version: 2022_02_18_171500) do
   create_table "tests_users", id: false, force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "test_id", null: false
-    t.index ["test_id"], name: "index_tests_users_on_test_id"
-    t.index ["user_id"], name: "index_tests_users_on_user_id"
+    t.index ["test_id", "user_id"], name: "index_tests_users_on_test_id_and_user_id"
+    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,4 +61,6 @@ ActiveRecord::Schema.define(version: 2022_02_18_171500) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests_users", "tests"
+  add_foreign_key "tests_users", "users"
 end
