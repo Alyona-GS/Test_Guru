af6fc158f2093d79fe3f0d81cba3871ba7f9126d
Alyona-GS <asgor1994@gmail.com>
2022-02-20 18:34:25 +0400

    refactor: Test.all_from(category) with joins

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,9 @@
 class Test < ApplicationRecord
 
+  belongs_to :category
   has_and_belongs_to_many :users
 
-  def self.all_from (category)
-    category_id = Category.find_by(title: category).id
-    self.where(category_id: category_id).pluck(:title).sort.reverse
-  rescue StandardError => e
-    puts "Category hasn't been found"
+  def self.all_from(category)
+    self.joins(:category).where('categories.title = ?', category).order(title: :desc).pluck(:title)
   end
 end
