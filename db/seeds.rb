177046e3c35ab527e0d5ccbcc4275f64536458a7
Alyona-GS <asgor1994@gmail.com>
2022-02-20 19:23:49 +0400

    refactor: foreign keys in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,40 @@
-User.create([
+users = User.create([
   { name: "Asgor", email: "my_email@mail.com" },
   { name: "Asmos", email: "my_email@mail.ru" } ])
 
-Category.create([
+categories = Category.create([
   { title: 'Ruby' },
   { title: 'HTML' },
   { title: 'Python' } ])
 
-Test.create([
-  { title: 'Control Flow',                        category_id: 1 },
-  { title: 'Metaprogramming',           level: 3, category_id: 1 },
-  { title: 'Text formatting',                     category_id: 2 },
-  { title: 'Dictionaries',              level: 2, category_id: 3 },
-  { title: 'Blocs, Procs, and Lambdas', level: 2, category_id: 1 } ])
+tests = Test.create([
+  { title: 'Control Flow',                        category_id: categories[0].id },
+  { title: 'Metaprogramming',           level: 3, category_id: categories[0].id },
+  { title: 'Text formatting',                     category_id: categories[1].id },
+  { title: 'Dictionaries',              level: 2, category_id: categories[2].id },
+  { title: 'Blocs, Procs, and Lambdas', level: 2, category_id: categories[0].id } ])
 
-Question.create([
-  { body: 'Is ternary operator applicable in Ruby?',       test_id: 1 },
-  { body: 'Could eval perform any string?',                test_id: 2 },
-  { body: 'What is tag <b> for?',                          test_id: 3 },
-  { body: 'What is the main loop used for dictionaries?',  test_id: 4 },
-  { body: 'Do procs have a similar behaviour as lambdas?', test_id: 5 } ])
+questions = Question.create([
+  { body: 'Is ternary operator applicable in Ruby?',       test_id: tests[0].id },
+  { body: 'Could eval perform any string?',                test_id: tests[1].id },
+  { body: 'What is tag <b> for?',                          test_id: tests[2].id },
+  { body: 'What is the main loop used for dictionaries?',  test_id: tests[3].id },
+  { body: 'Do procs have a similar behaviour as lambdas?', test_id: tests[4].id } ])
 
-Answer.create([
-  {body: "Yes",       correct: true, question_id: 1 },
-  {body: "Yes",       correct: true, question_id: 2 },
-  {body: "Bold font", correct: true, question_id: 3 },
-  {body: "For...in",  correct: true, question_id: 4 },
-  {body: "Yes, but lambda is more strict wuth arguments", correct: true, question_id: 5 },
+answers = Answer.create([
+  { body: "Yes",       correct: true, question_id: questions[0].id },
+  { body: "Yes",       correct: true, question_id: questions[1].id },
+  { body: "Bold font", correct: true, question_id: questions[2].id },
+  { body: "For...in",  correct: true, question_id: questions[3].id },
+  { body: "Yes, but lambda is more strict wuth arguments", correct: true, question_id: questions[4].id },
 
-  {body: "No",      question_id: 1 },
-  {body: "No",      question_id: 2 },
-  {body: "Cursive", question_id: 3 },
-  {body: "While",   question_id: 4 },
-  {body: "Procs is an alias of lambda", question_id: 5 },
+  { body: "No",      question_id: questions[0].id },
+  { body: "No",      question_id: questions[1].id },
+  { body: "Cursive", question_id: questions[2].id },
+  { body: "While",   question_id: questions[3].id },
+  { body: "Procs is an alias of lambda", question_id: questions[4].id },
 ])
 
-User.first.tests << Test.find(1)
-User.first.tests << Test.find(3)
-User.last.tests << Test.find(2)
+users.first.tests << tests[0]
+users.first.tests << tests[2]
+users.last.tests << tests[1]
