af4164e1842ca940194f8ccd5303575f15f88da8
Alyona-GS <asgor1994@gmail.com>
2022-02-22 06:03:57 +0400

    fix: rename columns, add migration

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,16 @@
 class Test < ApplicationRecord
 
   has_many :test_users
-  has_many :questions
   has_many :users, through: :test_users
+  has_many :questions
 
   belongs_to :category
-  belongs_to :user
+  belongs_to :author, class_name: "User"
 
   def self.all_from(category)
-    self.joins(:category).where('categories.title = ?', category).order(title: :desc).pluck(:title)
+    self.joins(:category).
+         where('categories.title = ?', category).
+         order(title: :desc).
+         pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
 
-  has_many :tests
+  has_many :own_tests, class_name: "Test", foreign_key: "author_id"
   has_many :tests_users
   has_many :tests, through: :tests_users
-
 
   def all_tests(level)
     self.tests.where(level: level)
