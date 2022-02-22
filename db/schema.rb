fe2b66ca9b583e9539af94c26dec46b42d84da8e
Alyona-GS <asgor1994@gmail.com>
2022-02-23 00:08:22 +0400

    fix: add not_null constraints to tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_012351) do
+ActiveRecord::Schema.define(version: 2022_02_22_195745) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -37,11 +37,11 @@ ActiveRecord::Schema.define(version: 2022_02_22_012351) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", default: 1
-    t.integer "category_id"
+    t.integer "level", default: 1, null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "author_id"
+    t.integer "author_id", null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
