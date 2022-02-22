38a7a613e0863c9366d4a736dcd85dcd2390e1ee
Alyona-GS <asgor1994@gmail.com>
2022-02-22 21:14:57 +0400

    feat: add validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,14 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+  validate :validate_quantity
+  validates_associated :question
 
   scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_quantity
+    errors.add(:question_id) unless (1..4).include?(Answer.where(question_id: question_id).count)
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -7,8 +7,10 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User"
 
-  validates :title, presence: true
-  validates :level, numericality: { only_integer: true }
+  validates :title, presence: true,
+                    uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 0 }
 
   scope :easy,   -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
