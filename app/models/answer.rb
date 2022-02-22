dd1b50f2b8d7f9930dc1deaf204c78ee3fd2a550
Alyona-GS <asgor1994@gmail.com>
2022-02-23 03:31:16 +0400

    refactor: Answer#validate_quantity

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,17 +3,12 @@ class Answer < ApplicationRecord
 
   validates :body, presence: true
   validate :validate_quantity
-  validates_associated :question
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_quantity
-    errors.add(:question_id) if too_much?
-  end
-
-  def too_much?
-    Answer.where(question_id: question_id).count > 3
+    errors.add(:question_id) if question.answers.count >= 4
   end
 end
