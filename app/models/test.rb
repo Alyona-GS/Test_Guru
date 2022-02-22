46bcd2d32d5b50fa6467f8561fdbc75c6f499cf1
Alyona-GS <asgor1994@gmail.com>
2022-02-23 02:27:28 +0400

    refactor: scopes from Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -12,17 +12,17 @@ class Test < ApplicationRecord
   validates :level, numericality: { only_integer: true,
                                     greater_than_or_equal_to: 0 }
 
-  scope :easy,   -> { where(level: 0..1) }
-  scope :medium, -> { where(level: 2..4) }
-  scope :hard,   -> { where(level: 5..Float::INFINITY) }
+  scope :easy,   -> { all_with_level(0..1) }
+  scope :medium, -> { all_with_level(2..4) }
+  scope :hard,   -> { all_with_level(5..Float::INFINITY) }
 
   scope :all_with_level, ->(level) { where(level: level) }
 
   scope :all_from, -> (category) {
-    joins(:category).
-    where('categories.title = ?', category).
-    order(title: :desc).
-    pluck(:title)
+    joins(:category).where('categories.title = ?', category)
   }
 
+  def self.title_from(category)
+    self.all_from(category).order(title: :desc).pluck(:title)
+  end
 end
