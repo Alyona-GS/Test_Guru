10072180d28fbfc4f7a533ff4bca92d588087db4
Alyona-GS <asgor1994@gmail.com>
2022-02-22 18:22:56 +0400

    feat: add scope methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,6 +7,10 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User"
 
+  scope :easy,   -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard,   -> { where(level: 5..Float::INFINITY) }
+
   def self.all_from(category)
     self.joins(:category).
          where('categories.title = ?', category).
